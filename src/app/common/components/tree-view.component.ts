9cb8690a5bfc1fa2a2f4a5a8ebc371edef659402
Fernando Vilhena <engesoft@hotmail.com>
2018-10-02 16:52:25 -0300

    ajustes no treeview menu

--- src/app/common/components/tree-view.component.ts
+++ src/app/common/components/tree-view.component.ts
@@ -6,11 +6,10 @@ import { MenuItemModel } from '../models/menu-item.model';
     template: `
     <mat-nav-list *ngFor="let node of treeData">
         <div (click)="toggleChildren(node)" style="width:calc(100% - 45px); float:left;">
-            <a mat-list-item routerLink="{{node.target}}">
-                {{node.title}}
-            </a>
+        <a mat-list-item *ngIf="node?.target == undefined" >{{node.title}}</a>
+        <a mat-list-item *ngIf="node?.target?.length >  0" routerLink="{{node.target}}">{{node.title}}</a>
         </div>
-        <div (click)="toggleChildren(node)" [ngClass]="{ has_item:node.children?.length}" style="float:right;width:41px;text-align:center;height: 32px; padding-top:4px;margin-top:4px;margin-bottom:8px">
+        <div id="toggleMenu" (click)="toggleChildren(node)" [ngClass]="{ has_item:node.children?.length}">
             <mat-icon style="vertical-align: center;" *ngIf="node.children?.length && !node.visible">expand_more</mat-icon>
             <mat-icon style="vertical-align: center;" *ngIf="node.children?.length && node.visible">expand_less</mat-icon>
         </div>
@@ -21,6 +20,16 @@ import { MenuItemModel } from '../models/menu-item.model';
   `,
     styles: [
         `
+            #toggleMenu{
+                float:right;
+                width:41px;
+                text-align:center;
+                height: 32px;
+                padding-top:4px;
+                margin-top:4px;
+                margin-bottom:8px;
+                cursor: pointer;
+            }
             .has_item {
                 border-left: 1px solid silver;
             }
